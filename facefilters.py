80f345ebf811ff2dd2e87b8dd54b5f2a17a8386f
digi0ps <sriru1998@gmail.com>
2018-11-09 21:36:01 +0530

    Split sprite operations into funcitons.

--- facefilters.py
+++ facefilters.py
@@ -3,6 +3,30 @@ import numpy as np
 import face_recognition as face
 
 
+def apply_sprite(sprite, rows, cols):
+    seg = frame[rows[0]*4:rows[1]*4, cols[0]*4: cols[1]*4]
+    r, c, _ = seg.shape
+    print(rows, cols)
+    re_sprite = cv2.resize(sprite, (c, r))
+
+    img2gray = cv2.cvtColor(re_sprite, cv2.COLOR_BGR2GRAY)
+
+    ret, mask = cv2.threshold(img2gray, 5, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+
+    img1_bg = cv2.bitwise_and(seg, seg, mask=mask_inv)
+    img2_fg = cv2.bitwise_and(re_sprite, re_sprite, mask=mask)
+    dst = cv2.add(img1_bg, img2_fg)
+    frame[rows[0]*4:rows[1]*4, cols[0]*4: cols[1]*4] = dst
+
+
+def add_nose_sprite(nose_sprite):
+    nose_tip = face_landmarks[0]['nose_tip']
+    nose_bridge = face_landmarks[0]['nose_bridge']
+    apply_sprite(nose_sprite,
+                 (nose_bridge[2][1], nose_tip[0][1]), (nose_tip[0][0], nose_tip[4][0]))
+
+
 doggy_nose = cv2.imread("./sprites/doggy_nose.png")
 frame = cv2.imread("./sprites/Pic.jpg")
 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -17,28 +41,13 @@ for top, right, bottom, left in face_locations:
     cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
 
 face_landmarks = []
-# Do face facial recognition only if a face is detected
+
 if len(face_locations):
     face_landmarks = face.face_landmarks(small_frame)
-# gives a list containing a dictionary with features as key, coordinates as values
-#print(face_landmarks)
-print(face_landmarks[0]['nose_tip']);
-print(face_landmarks[0]['nose_bridge']);
-seg = frame[69*4:80*4, 123*4:140*4]
-rows, cols, s = seg.shape
-nose = cv2.resize(doggy_nose, (cols,rows));
 
-img2gray = cv2.cvtColor(nose,cv2.COLOR_BGR2GRAY)
-
-ret, mask = cv2.threshold(img2gray, 5, 255, cv2.THRESH_BINARY)
-mask_inv = cv2.bitwise_not(mask)
-
-img1_bg = cv2.bitwise_and(seg, seg, mask = mask_inv)
-img2_fg = cv2.bitwise_and(nose,nose,mask = mask)
-dst = cv2.add(img1_bg,img2_fg)
-frame[69*4:80*4, 123*4:140*4] = dst
+add_nose_sprite(doggy_nose)
 
 cv2.imshow('Face Rec', frame)
-cv2.imwrite( "filter.jpg", frame)
+cv2.imwrite("filter.jpg", frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
